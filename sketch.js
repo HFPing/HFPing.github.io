68bc18a245cc505c2ea48fff831f560a2c6e5caf
HFPing <hfp.ing2@gmail.com>
2017-12-29 13:17:32 -0600

    Dibujo

--- sketch.js
+++ sketch.js
@@ -1,26 +1,17 @@
-var pointX={
-	x:100,
-	y:50
-};
-
-var col={
-	r:255,
-	g:0,
-	b:0
-};
+// background, stroke, fill
 
 function setup() {
 	createCanvas(600,400);
-	background(0);
+	background(250,250,100);
+}
+// Comentario
+function draw() {
+	//background(250,250,100);
+	noStroke();
+	fill(250,200,200,50);
+	ellipse(mouseX,mouseY,25,25); // Coordenadas del mouse
 }
 
-function draw() {
-	pointX.x=random(0,600);
-	pointX.y=random(0,400);
-	col.r=random(100,255);
-	col.g=0;
-	col.b=random(100,190);
-	noStroke();
-	fill(col.r,col.g,col.b,100);
-	ellipse(pointX.x,pointX.y,24,24); 
+function mousePressed(){
+	background(250,250,100);
 }
